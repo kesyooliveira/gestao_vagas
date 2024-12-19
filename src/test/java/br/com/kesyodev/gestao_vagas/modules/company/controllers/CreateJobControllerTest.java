6c2db2e66de5401bcf6e70293938a31cb4f94320
Kesyo <kesyooliveira@gmail.com>
2024-12-18 22:04:36 -0300

    criando h2db para testes

--- src/test/java/br/com/kesyodev/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/kesyodev/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -1,6 +1,8 @@
 package br.com.kesyodev.gestao_vagas.modules.company.controllers;
 
 import br.com.kesyodev.gestao_vagas.modules.company.dto.CreateJobDTO;
+import br.com.kesyodev.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.kesyodev.gestao_vagas.modules.company.repositories.CompanyRepository;
 import br.com.kesyodev.gestao_vagas.utils.TestUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,18 +19,21 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.UUID;
 
 import static br.com.kesyodev.gestao_vagas.utils.TestUtils.objectToJSON;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CreateJobControllerTest {
 
     private MockMvc mvc;
 
     @Autowired
     private WebApplicationContext context;
+
+    @Autowired
+    private CompanyRepository companyRepository;
 
     @BeforeEach()
     public void setup(){
@@ -37,12 +43,22 @@ public class CreateJobControllerTest {
     @Test
     public void shouldBeAbleToCreateNewJob() throws Exception {
 
+        var company = CompanyEntity.builder()
+                .Description("COMPANY_DESCRIPTION")
+                .email("EMAIL@COMPANY.COM")
+                .password("1234567890")
+                .username("COMPANY_USERNAME")
+                .name("COMPANY_NAME")
+                .build();
+
+        companyRepository.saveAndFlush(company);
+
         var createJobDTO = CreateJobDTO.builder().benefits("BENEFITS_TEST").description("DESCRIPTION_TEST").level("LEVEL_TEST").build();
 
         var result = mvc.perform(MockMvcRequestBuilders.post("/company/job/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectToJSON(createJobDTO))
-                        .header("Authorization", TestUtils.generateToken(UUID.fromString("f2c2ce5f-0a9c-432e-96ca-b72a4af70af3"),"my-secret-key")))
+                        .header("Authorization", TestUtils.generateToken(company.getId(),"my-secret-key")))
                 .andExpect(MockMvcResultMatchers.status().isOk());
         System.out.println(result);
 
